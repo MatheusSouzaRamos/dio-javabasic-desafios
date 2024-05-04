ac9aa56d56916b452bc046ececc73cc8291792b2
MatheusSouzaRamos <strikman7@gmail.com>
2024-05-04 16:14:56 -0300

    contabanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main (String [] args) throws Exception{
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite a Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponícel para saque.");
+
+    }
+
+}
